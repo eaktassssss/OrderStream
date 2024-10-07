8a59bb7259cdfc16800996f48111b61f3787fa23
Evren Aktaş <evren.aktas@nesine.com>
2024-10-07 22:09:26 +0300

    Mock,Fixture added

---
+++ OrderStream.Tests/Fixtures/ProductServiceTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using OrderStream.Application.Interfaces.Repositories;
+using OrderStream.Infrastructure.Implementations.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderStream.Tests.Fixtures
+{
+    public  class ProductServiceTestFixture
+    {
+        Mock<IProductRepository> ProductRepositoryMock;
+        public ProductService ProductService { get; private set; }
+        public ProductServiceTestFixture()
+        {
+            ProductRepositoryMock= new Mock<IProductRepository> ();
+            ProductService= new ProductService(ProductRepositoryMock.Object);
+        }
+    }
+}
---
+++ OrderStream.Tests/ServiceTests/ProductServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using OrderStream.Application.Interfaces.Repositories;
+using OrderStream.Application.Models;
+using OrderStream.Application.Services;
+using OrderStream.Domain.Entities;
+using OrderStream.Infrastructure.Implementations.Services;
+using OrderStream.Tests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderStream.Tests.ServiceTests
+{
+    public class ProductServiceTests:IClassFixture<ProductServiceTestFixture>
+    {
+        private readonly IProductService _productService;
+        public ProductServiceTests(ProductServiceTestFixture fixture)
+        {
+            _productService=fixture.ProductService;
+        }
+
+        [Fact]
+        public void CreateProduct_ProductName_NullOrWhiteSpace_ReturnFalse()
+        {
+            //Arrange
+            ProductModel model = new ProductModel();
+            //Act
+            var result = _productService.CreateProduct(model);
+            Assert.False(result);
+        }
+    }
+}
