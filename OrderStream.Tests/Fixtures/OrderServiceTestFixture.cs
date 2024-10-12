d59f27bec3cfd2f920ec4999de6bfc8b1f9ed7f9
evren <evren@EVRENAKTAS>
2024-10-13 02:32:43 +0300

    OrderServiceTestFixture added project

---
+++ OrderStream.Tests/Fixtures/OrderServiceTestFixture.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using OrderStream.Application.Interfaces.Repositories;
+using OrderStream.Infrastructure.Implementations.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderStream.Tests.Fixtures
+{
+    public class OrderServiceTestFixture
+    {
+        public readonly Mock<IOrderRepository> 
+            OrderRepositoryMock;
+        public readonly Mock<IProductRepository> 
+             ProductRepositoryMock;
+
+        public OrderService OrderService;
+        public OrderServiceTestFixture()
+        {
+            ProductRepositoryMock = new Mock<IProductRepository>();
+            OrderRepositoryMock = new Mock<IOrderRepository>();
+            OrderService = new OrderService(OrderRepositoryMock.Object, ProductRepositoryMock.Object);
+        }
+
+    }
+}
---
+++ OrderStream.Tests/ServiceTests/OrderServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using OrderStream.Tests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderStream.Tests.ServiceTests
+{
+    public class OrderServiceTests:IClassFixture<OrderServiceTestFixture>
+    {
+        private readonly OrderServiceTestFixture _fixture;
+        public OrderServiceTests(OrderServiceTestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+    }
+}
