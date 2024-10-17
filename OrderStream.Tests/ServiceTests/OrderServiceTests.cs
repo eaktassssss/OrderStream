0a3c5ce575ba1e1e90882ba8717438c30adf177d
evren <evren@EVRENAKTAS>
2024-10-17 23:31:49 +0300

    OrderService  => DeleteOrder unit tests

--- OrderStream.Tests/ServiceTests/OrderServiceTests.cs
+++ OrderStream.Tests/ServiceTests/OrderServiceTests.cs
@@ -200,9 +200,14 @@ namespace OrderStream.Tests.ServiceTests
         public void GetOrderById_InvalidOrder_ReturnOrder()
         {
             string id = "66f037e7a04b71bfcaad6487";
+
+            var order=_fixture.CreateModelInstance<Order>();
+            order = new Order { Id = id, CustomerId = 1, OrderDate = DateTime.Now, OrderStatus = Domain.Enums.OrderStatus.Completed, TotalAmount = 1000, OrderItems = new List<OrderItem> { new OrderItem { Price = 50, ProductId = "66f037e7a04b71bfcaad6483", Quantity = 20 } } };
+
+
             _fixture.OrderRepositoryMock
                 .Setup(x => x.GetById(id))
-                .Returns(new Order { Id=id,CustomerId=1,OrderDate=DateTime.Now,OrderStatus=Domain.Enums.OrderStatus.Completed,TotalAmount=1000,OrderItems= new List<OrderItem> { new OrderItem { Price=50,ProductId= "66f037e7a04b71bfcaad6483",Quantity=20 } } });
+                .Returns(order);
 
             var result = _fixture.OrderService.GetOrderById(id);
 
@@ -241,9 +246,12 @@ namespace OrderStream.Tests.ServiceTests
         public void CancelOrder_ValidOrder_ReturnTrue()
         {
             string id = "66f037e7a04b71bfcaad6487";
+            var order = _fixture.CreateModelInstance<Order>();
+            order = new Order { Id = id, CustomerId = 1, OrderDate = DateTime.Now, OrderStatus = Domain.Enums.OrderStatus.Completed, TotalAmount = 1000, OrderItems = new List<OrderItem> { new OrderItem { Price = 50, ProductId = "66f037e7a04b71bfcaad6483", Quantity = 20 } } };
+           
             _fixture.OrderRepositoryMock
                 .Setup(x => x.GetById(id))
-                .Returns(new Order { Id = id, CustomerId = 1, OrderDate = DateTime.Now, OrderStatus = Domain.Enums.OrderStatus.Completed, TotalAmount = 1000, OrderItems = new List<OrderItem> { new OrderItem { Price = 50, ProductId = "66f037e7a04b71bfcaad6483", Quantity = 20 } } });
+                .Returns(order);
 
             _fixture.OrderRepositoryMock
                 .Setup(x => x.Update(It.IsAny<Order>()))
@@ -255,5 +263,51 @@ namespace OrderStream.Tests.ServiceTests
         }
         #endregion
 
+        #region DeleteOrder 
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void DeleteOrder_OrderId_IsNullOrEmpty_ReturnFalse(string id)
+        {
+            var result = _fixture.OrderService.DeleteOrder(id);
+
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("66f037e7a04b71bfcaad6487")]
+        public void DeleteOrder_NotExistingOrder_ReturnFalse(string id)
+        {
+            _fixture.OrderRepositoryMock
+                .Setup(x => x.GetById(It.IsAny<string>()))
+                .Returns((Order)null);
+
+            var result = _fixture.OrderService.DeleteOrder(id);
+
+            Assert.False(result);
+        }
+
+
+        [Theory]
+        [InlineData("66f037e7a04b71bfcaad6487")]
+        public void DeleteOrder_ValidOrder_ReturnTrue(string id)
+        {
+
+             var order = _fixture.CreateModelInstance<Order>();
+              order = new Order { Id = id, CustomerId = 1, OrderDate = DateTime.Now, OrderStatus = Domain.Enums.OrderStatus.Completed, TotalAmount = 1000, OrderItems = new List<OrderItem> { new OrderItem { Price = 50, ProductId = "66f037e7a04b71bfcaad6483", Quantity = 20 } } };
+
+
+            _fixture.OrderRepositoryMock
+                .Setup(x => x.GetById(It.IsAny<string>()))
+                .Returns(order);
+
+            _fixture.OrderRepositoryMock.Setup(x => x.Delete(It.IsAny<string>()))
+                .Returns(true);
+
+            var result = _fixture.OrderService.DeleteOrder(id);
+
+            Assert.True(result);
+        }
+        #endregion
     }
 }
