dcd94995e4044e634b136982edd6c9744e8cb807
evren <evren@EVRENAKTAS>
2024-10-17 23:23:07 +0300

    OrderService => CancelOrder unit tests

--- OrderStream.Tests/ServiceTests/OrderServiceTests.cs
+++ OrderStream.Tests/ServiceTests/OrderServiceTests.cs
@@ -211,5 +211,49 @@ namespace OrderStream.Tests.ServiceTests
         }
         #endregion
 
+        #region CancelOrder
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void CancelOrder_IsNullOrEmpty_ReturnFalse(string id)
+        {
+            var result = _fixture.OrderService.CancelOrder(id);
+
+            Assert.False(result);
+        }
+
+
+        [Theory]
+        [InlineData("66f037e7a04b71bfcaad6487")]
+        public void CancelOrder_NotExistingOrder_ReturnFalse(string id)
+        {
+            _fixture.OrderRepositoryMock
+                .Setup(x => x.GetById(It.IsAny<string>()))
+                .Returns((Order)null);
+
+            var result = _fixture.OrderService.CancelOrder(id);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void CancelOrder_ValidOrder_ReturnTrue()
+        {
+            string id = "66f037e7a04b71bfcaad6487";
+            _fixture.OrderRepositoryMock
+                .Setup(x => x.GetById(id))
+                .Returns(new Order { Id = id, CustomerId = 1, OrderDate = DateTime.Now, OrderStatus = Domain.Enums.OrderStatus.Completed, TotalAmount = 1000, OrderItems = new List<OrderItem> { new OrderItem { Price = 50, ProductId = "66f037e7a04b71bfcaad6483", Quantity = 20 } } });
+
+            _fixture.OrderRepositoryMock
+                .Setup(x => x.Update(It.IsAny<Order>()))
+                .Returns(true);
+
+            var result = _fixture.OrderService.CancelOrder(id);
+
+            Assert.True(result);
+        }
+        #endregion
+
     }
 }
